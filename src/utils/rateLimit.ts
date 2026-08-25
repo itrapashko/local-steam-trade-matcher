554a9bbaa7e1f0132c8a1efb7938bdc5d7740158
Ilya Trapashko <miningilya2@gmail.com>
2026-08-25 22:47:40 +0300

    Reduce time between requests to 100 ms

--- src/utils/rateLimit.ts
+++ src/utils/rateLimit.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_RATE_LIMIT_MS = 300
+export const DEFAULT_RATE_LIMIT_MS = 100
 
 export function delay(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms))
